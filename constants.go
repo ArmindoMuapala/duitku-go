1a71e03c1514c0c31d229f7e5b0034bd1dc7b4b2
fatkulnurk <fatkulnurk@gmail.com>
2025-06-01 07:32:47 +0700

    feat: update payment method codes and add example credentials in main.go

--- constants.go
+++ constants.go
@@ -2,53 +2,65 @@ package duitku
 
 // Payment method constants
 const (
-	// Bank Transfer
-	PaymentMethodBCA      = "BC" // BCA Virtual Account
-	PaymentMethodMandiri  = "M1" // Mandiri Virtual Account
-	PaymentMethodPermata  = "BT" // Permata Virtual Account
-	PaymentMethodBNI      = "I1" // BNI Virtual Account
-	PaymentMethodBRI      = "BR" // BRI Virtual Account
-	PaymentMethodCIMB     = "B1" // CIMB Niaga Virtual Account
-	PaymentMethodDanamon  = "DN" // Danamon Virtual Account
-	PaymentMethodMaybank  = "VA" // Maybank Virtual Account
-	PaymentMethodSahabat  = "SA" // Sahabat Sampoerna Virtual Account
-	PaymentMethodBSI      = "S1" // BSI Virtual Account
-	
-	// E-Wallet
-	PaymentMethodOVO      = "OV" // OVO
-	PaymentMethodShopeePay = "SP" // ShopeePay
-	PaymentMethodLinkAja  = "LA" // LinkAja
-	PaymentMethodDANA     = "DA" // DANA
-	
-	// QRIS
-	PaymentMethodQRIS     = "QR" // QRIS
-	
-	// Retail Outlets
-	PaymentMethodAlfamart = "A1" // Alfamart
-	PaymentMethodIndomaret = "IR" // Indomaret
-	
 	// Credit Card
 	PaymentMethodCreditCard = "VC" // Credit Card
-	
-	// Paylater
-	PaymentMethodAkulaku  = "AK" // Akulaku
-	PaymentMethodKredivo  = "K1" // Kredivo
-	PaymentMethodAtome    = "AT" // Atome
+
+	// Virtual Account
+	PaymentMethodBCA         = "BC" // BCA Virtual Account
+	PaymentMethodMandiri     = "M2" // Mandiri Virtual Account
+	PaymentMethodMaybank     = "VA" // Maybank Virtual Account
+	PaymentMethodBNI         = "I1" // BNI Virtual Account
+	PaymentMethodCIMB        = "B1" // CIMB Niaga Virtual Account
+	PaymentMethodPermata     = "BT" // Permata Bank Virtual Account
+	PaymentMethodATMBersama  = "A1" // ATM Bersama
+	PaymentMethodArthaGraha  = "AG" // Bank Artha Graha / Bank Artha Graha Virtual Account
+	PaymentMethodNeoCommerce = "NC" // Bank Neo Commerce / BNC / Bank Neo Commerce Virtual Account
+	PaymentMethodBRI         = "BR" // BRIVA / Bank BRI / Bank Rakyat Indonesia Virtual Account
+	PaymentMethodSahabat     = "S1" // Bank Sahabat Sampoerna / Bank Sahabat Sampoerna Virtual Account
+	PaymentMethodDanamon     = "DM" // Danamon Virtual Account
+	PaymentMethodBSI         = "BV" // BSI Virtual Account
+
+	// Retail Outlets
+	PaymentMethodAlfamart  = "FT" // Alfamart (Pegadaian/ALFA/Pos)
+	PaymentMethodPegadaian = "FT" // Pegadaian (Pegadaian/ALFA/Pos)
+	PaymentMethodPOS       = "FT" // POS (Pegadaian/ALFA/Pos)
+	PaymentMethodIndomaret = "IR" // Indomaret
+
+	// E-Wallet
+	PaymentMethodOVO            = "OV" // OVO (Support Void)
+	PaymentMethodShopeePay      = "SA" // Shopee Pay Apps (Support Void)
+	PaymentMethodLinkAjaFixed   = "LF" // LinkAja Apps (Fixed Fee)
+	PaymentMethodLinkAjaPercent = "LA" // LinkAja Apps (Percentage Fee)
+	PaymentMethodDANA           = "DA" // DANA
+	PaymentMethodShopeeLink     = "SL" // Shopee Pay Account Link
+	PaymentMethodOVOLink        = "OL" // OVO Account Link
+	PaymentMethodJeniusPay      = "JP" // Jenius Pay
+
+	// QRIS
+	PaymentMethodQrisShopeePay     = "SP" // QRIS ShopeePay
+	PaymentMethodQrisNobu          = "QN" // QRIS Nobu
+	PaymentMethodQrisDana          = "DQ" // QRIS Dana
+	PaymentMethodQrisGudangVoucher = "GQ" // QRIS Gudang Voucher
+	PaymentMethodQrisNusapay       = "SQ" // QRIS Nusapay
+
+	// Paylater/Credit
+	PaymentMethodIndodanaPaylater = "ID" // Indodana Paylater
+	PaymentMethodAtome            = "AT" // ATOME
 )
 
 // Transaction status codes
 const (
-	StatusSuccess         = "00" // Success
-	StatusPending         = "01" // Pending
-	StatusFailed          = "02" // Failed
-	StatusCancelled       = "03" // Cancelled
-	StatusExpired         = "04" // Expired
+	StatusSuccess   = "00" // Success
+	StatusPending   = "01" // Pending
+	StatusFailed    = "02" // Failed
+	StatusCancelled = "03" // Cancelled
+	StatusExpired   = "04" // Expired
 )
 
 // Subscription frequency types
 const (
-	FrequencyDaily   = 1
-	FrequencyWeekly  = 2
-	FrequencyMonthly = 3
-	FrequencyYearly  = 4
+	FrequencyDaily   = 1 // Daily
+	FrequencyWeekly  = 2 // Weekly
+	FrequencyMonthly = 3 // Monthly
+	FrequencyYearly  = 4 // Yearly
 )
